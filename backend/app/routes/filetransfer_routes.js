ab21754a17fe00472ff5d1b642fb8b3da7d59f92
guillim <guigloo@msn.com>
2018-10-08 17:34:09 +0200

    on change les endpoint de l'API. en effet avant on avait api.previsecours.fr/api/status maintenant on aura juste api.previsecours.fr/status

--- backend/app/routes/filetransfer_routes.js
+++ backend/app/routes/filetransfer_routes.js
@@ -3,25 +3,25 @@ const jsonResponse = require('./jsonResponse.js')
 
 module.exports = function(app) {
   // first route, a simple GET to know it the server is ON
-  app.get('/api/status', (req, res) => {
+  app.get('/status', (req, res) => {
     console.log('server side log through docker');
     jsonResponse(200,{status:"Good",detail: 'Connected to the server (served through docker)'})(res)
   });
   // second route, a simple GET to know it the accessToken works
-  app.get('/api/AuthentificationStatus', (req, res) => {
+  app.get('/AuthentificationStatus', (req, res) => {
     const accessToken = req.headers['x-access-token'] || false;
     if (accessToken !== process.env.accessToken) {
-      jsonResponse(400,{status:"Error",detail: 'Authentification failed. Please contact support.'})(res)
+      jsonResponse(401,{status:"Error",detail: 'Authentification failed. Please contact support.'})(res)
     }else{
       jsonResponse(200,{status:"Good",detail: 'Authentification validated.'})(res)
     }
   });
 
   // third route, to post a file
-  app.post('/api/uploadFile/interventions/:dpt', (req, res) => {
+  app.post('/uploadFile/interventions/:dpt', (req, res) => {
     const accessToken = req.headers['x-access-token'] || false;  //important car si process.env.accessToken === undefined, faut pas une egalite
     if (accessToken !== process.env.accessToken) {
-      jsonResponse(400,{status:"Error",detail: 'Authentification failed. Please contact support.'})(res)
+      jsonResponse(401,{status:"Error",detail: 'Authentification failed. Please contact support.'})(res)
     }
 
     const dpt = (req.params.dpt) ? req.params.dpt : false
